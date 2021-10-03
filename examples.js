6af8a21b003af8803b98821d28390beb91dab32f
Ezra Oppenheimer <78681534+Heavenira@users.noreply.github.com>
2021-10-03 13:56:32 -0400

    Create examples.js

---
+++ examples.js
@@ -0,0 +1,46 @@
+desmos2ascii(String.raw`\frac{1}{\sqrt{x}}`)
+
+desmos2ascii(String.raw`
+x=-\frac{\left(-1478412\pi a^{2}f_{57}+\sqrt{\left(-1478412\pi a^{2}f_{57}-54\right)^{2}-2916}-54\right)^{\frac{1}{3}}}{702\cdot2^{\frac{1}{3}}a}-\frac{1}{39\cdot2^{\frac{2}{3}}a\left(-1478412\pi a^{2}f_{57}+\sqrt{\left(-1478412\pi a^{2}f_{57}-54\right)^{2}-2916}-54\right)^{\frac{1}{3}}}+\frac{1}{234a}`)
+
+desmos2ascii(String.raw`1-\gamma\left(x-1\right)+\frac{1}{12}\left(6\gamma^{2}+\pi^{2}\right)\left(x-1\right)^{2}+\frac{1}{6}\left(x-1\right)^{3}\left(-\gamma^{3}-\frac{\gamma\pi^{2}}{2}+\psi_{poly}\left(2,1\right)\right)+\frac{1}{24}\left(x-1\right)^{4}\left(\gamma^{4}+\gamma^{2}\pi^{2}+\frac{3\pi^{4}}{20}-4\gamma\psi_{poly}\left(2,1\right)\right)+\frac{1}{120}\left(x-1\right)^{5}\left(-\gamma^{5}-\frac{5\gamma^{3}\pi^{2}}{3}-\frac{3\gamma\pi^{4}}{4}+10\gamma^{2}\psi_{poly}\left(2,1\right)+\frac{5\pi^{2}\psi_{poly}\left(2,1\right)}{3}+\psi_{poly}\left(4,1\right)\right)+O\left(\left(x-1\right)^{6}\right)`)
+
+desmos2ascii(String.raw`B_{n}=\operatorname{nCr}\left(\sum_{m=0}^{n}\frac{\left(-1\right)^{m}\sum_{i=0}^{-1+m}\left(-1\right)^{i}\left(-i+m\right)^{n}\operatorname{nCr}\left(m,i\right)}{1+m},4\right)`)
+
+desmos2ascii(String.raw`\operatorname{floor}\left(\frac{2x-4x_{3}}{5}\right)`)
+
+desmos2ascii(String.raw`2\left(1+\left(\frac{1}{x}\operatorname{mod}\left(\pi,3\right)6f\left(x\right)+1\right)\cdot4\right)`)
+
+desmos2ascii(String.raw`\Upsilon_{preFinal}\left(x_{Median},y_{Median},z_{Median},x_{NORMAL},y_{NORMAL},z_{NORMAL},x_{CamOffset},y_{CamOffset},z_{CamOffset},x_{LightOffset},y_{LightOffset},z_{LightOffset},r_{adius}\right)=\left(\left(x_{CamOffset}x_{NORMAL}+y_{CamOffset}y_{NORMAL}+z_{CamOffset}z_{NORMAL}\right)\left(x_{LightOffset}x_{NORMAL}+y_{LightOffset}y_{NORMAL}+z_{LightOffset}z_{NORMAL}\right)-\frac{\left(x_{CamOffset}x_{NORMAL}z_{NORMAL}+y_{CamOffset}y_{NORMAL}z_{NORMAL}-z_{CamOffset}\left(x_{NORMAL}^{2}+y_{NORMAL}^{2}\right)\right)\left(x_{LightOffset}x_{NORMAL}z_{NORMAL}+y_{LightOffset}y_{NORMAL}z_{NORMAL}-z_{LightOffset}\left(x_{NORMAL}^{2}+y_{NORMAL}^{2}\right)\right)+\left(y_{CamOffset}x_{NORMAL}-x_{CamOffset}y_{NORMAL}\right)\left(y_{LightOffset}x_{NORMAL}-x_{LightOffset}y_{NORMAL}\right)}{x_{NORMAL}^{2}+y_{NORMAL}^{2}}\right)^{2}-\left(x_{CamOffset}^{2}+y_{CamOffset}^{2}+z_{CamOffset}^{2}\right)\left(x_{LightOffset}^{2}+y_{LightOffset}^{2}+z_{LightOffset}^{2}-r_{adius}\right)`)
+
+desmos2ascii(String.raw`\rho\left(x,y\right)=\left(x.x,x.y+\left[0...y\right]0\right)`)
+
+desmos2ascii(String.raw`y_{semicircle}\left(x,y\right)=\left\{x<180:\sin x,180\le x<360:0,360\le x<540:y\sin x,540\le x:0\right\}`)
+
+desmos2ascii(String.raw`\left(x_{square}\left(t\right)+\sum_{n=1}^{\operatorname{floor}\left(t\right)}\frac{1}{2n},N-\left(y_{square}\left(t\right)+\operatorname{floor}\left(t\right)\right)\right)`)
+
+desmos2ascii(String.raw`\operatorname{mod}\left(y-\left[0,2,0,2\right],4\right)=-1.2\left(\left\{\left[0.32,0.32,1.28,1.28\right]<\operatorname{mod}\left(x-\left[0,1.3,0,1.3\right],2.63\right)<\left[0.72,0.72,1.68,1.68\right]:\operatorname{mod}\left(x-\left[0,1.3,0,1.3\right],2.63\right)\right\}-\left[0.76,0.76,1.24,1.24\right]\right)^{2}+1.67`)
+
+desmos2ascii(String.raw`F_{GridSquare}\left(x,i\right)=F_{GridGlobalScale}\cdot2\left(\min\left(1,\max\left(0,1.5-\left|4x-1.5\right|\right)\right)-\frac{1}{2},\min\left(1,\max\left(0,1.5-\left|4x-2.5\right|\right)\right)-\frac{1}{2}\right)+F_{GridCoordinate}\left(i\right)`)
+
+desmos2ascii(String.raw`\operatorname{mod}\left(2\arctan\left(x,45\right),3\right)`)
+
+desmos2ascii(String.raw`V\left(\left(0-x_{LightFinal}\right)\frac{-z_{LightFinal}}{n\left(1+y_{square}\left(t\right)\right)-z_{LightFinal}}+x_{LightFinal},\left(nx_{square}\left(t\right)-y_{LightFinal}\right)\frac{-z_{LightFinal}}{n\left(1+y_{square}\left(t\right)\right)-z_{LightFinal}}+y_{LightFinal},0\right)`)
+
+desmos2ascii(String.raw`F\left(R\left(q^{L}S_{quare}\left(t\right),L\left(\frac{\pi}{2}-l\right)\right)+\left(\sum_{n=0}^{L-1}R\left(q^{n}S_{quare}\left(0.75\right),n\left(\frac{\pi}{2}-l\right)\right).x,\sum_{n=0}^{L-1}R\left(q^{n}S_{quare}\left(0.75\right),n\left(\frac{\pi}{2}-l\right)\right).y\right)\right)`)
+
+desmos2ascii(String.raw`x=f\left(f\left(f\left(f\left(c_{heckpoInt}\right)\right)\right)\right)+\frac{y+17}{\frac{-19+17}{f\left(f\left(f\left(f\left(f\left(c_{heckpoInt}\right)\right)\right)\right)\right)-f\left(f\left(f\left(f\left(c_{heckpoInt}\right)\right)\right)\right)}}\left\{-19\le y\le-17\right\}`)
+
+desmos2ascii(String.raw`S_{tereographic}\left(2\cos\left(U_{nits2}\beta\left(l_{ist}\right)\ +\ U_{nits2}\right),\ 2\sin\left(U_{nits2}\beta\left(l_{ist}\right)\ +\ U_{nits2}\right),\ 2\cos\left(U_{nits2}\alpha\left(l_{ist}\right)\ +\ U_{nits2}\right),\ 2\sin\left(U_{nits2}\alpha\left(l_{ist}\right)\ +\ U_{nits2}\right)\right)`)
+
+desmos2ascii(String.raw`\frac{6\sin\left(\pi x\right)+5\pi x^{2}\sin^{2}\left(\frac{\pi x}{2}\right)-3\pi x\sin^{-1}\left(\frac{\pi x}{2}\right)-2\pi\sin^{2}\left(\frac{\pi x}{2}\right)+\pi x^{2}-\pi x}{2\pi x-2\pi}`)
+
+desmos2ascii(String.raw`\sin\left(\operatorname{mod}\left(x,\frac{324x}{x^{3}-1}\right)\right)`)
+
+desmos2ascii(String.raw`\log_{e}6+4x\le5`)
+
+desmos2ascii(String.raw`\int_{1}^{y\left(x\right)}\left(\frac{1}{\sqrt{2\ln\left(x^{2}+\left(3-2\zeta\right)x+\left(\zeta-3\right)\zeta\right)+2c_{1}+1}}-\int_{1}^{x}\frac{2\zeta-2\xi-3}{\left(\xi^{2}+\left(3-2\zeta\right)\xi+\left(\zeta-3\right)\zeta\right)\left(2\ln\left(\xi^{2}+\left(3-2\zeta\right)\xi+\left(\zeta-3\right)\zeta\right)+2c_{1}+1\right)^{\frac{3}{2}}}d\xi\right)d\zeta+\int_{1}^{x}\left(1-\frac{1}{\sqrt{2\ln\left(\xi^{2}-2y\left(x\right)\xi+3\xi+y\left(x\right)^{2}-3y\left(x\right)\right)+2c_{1}+1}}\right)d\xi=c_{2}`)
+
+desmos2ascii(String.raw`O_{bs0}\left(x,y,z\right)=\sum_{i=0}^{s_{teps}}\left\{M_{esh0}\left(\Delta_{Ray}\left(x,x_{CameraFinal},i\right),\Delta_{Ray}\left(y,y_{CameraFinal},i\right),\Delta_{Ray}\left(z,z_{CameraFinal},i\right)\right)=0:0\right\}`)
+
+desmos2ascii(String.raw`L_{matrix}=\left\{\sqrt{\operatorname{length}\left(M_{00}\right)}=\operatorname{round}\left(\sqrt{\operatorname{length}\left(M_{00}\right)}\right):\sqrt{\operatorname{length}\left(M_{00}\right)}\right\}`)
